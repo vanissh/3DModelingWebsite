c9d70dad1607d59f633d65b8d1477bc919766494
Anna Kuznetsova <sutcliff0684@gmail.com>
2020-12-15 14:43:07 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+
+    //timer
+    function countTimer(deadline){
+        let timerHours = document.querySelector('#timer-hours'), 
+            timerMinutes = document.querySelector('#timer-minutes'), 
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60),
+                minutes = Math.floor((timeRemaining / 60) % 60),
+                hours = Math.floor((timeRemaining / 3600));
+
+                return {timeRemaining, hours, minutes, seconds};
+        }
+
+        let idInterval;
+        
+        function updateClock(){
+            let timer = getTimeRemaining();
+
+            for(let key in timer){
+                if(key !== 'timeRemaining' && timer[key] < 10){
+                    timer[key] = '0' + timer[key];
+                }
+            }
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+
+            if(timer.timeRemaining < 0){
+                clearInterval(idInterval);
+                
+                timerHours.textContent = '00';
+                timerMinutes.textContent  = '00';
+                timerSeconds.textContent = '00';
+
+                console.log(timerHours.innerHTML);
+            }
+            
+        }
+
+        idInterval = setInterval(updateClock, 1000);
+
+    }
+
+    countTimer('16 december 2020');
+    
+});
